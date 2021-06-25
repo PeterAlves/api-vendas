65e4391d637ebc803320dd0610cda77d553d2790
Peterson Alves dos Santos <peterhospdis@gmail.com>
2021-06-25 16:20:33 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/peterAlves/apiVendas/services/PedidoService.java
+++ src/main/java/com/peterAlves/apiVendas/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.peterAlves.apiVendas.domain.Cliente;
 import com.peterAlves.apiVendas.domain.ItemPedido;
 import com.peterAlves.apiVendas.domain.PagamentoComBoleto;
 import com.peterAlves.apiVendas.domain.Pedido;
@@ -13,6 +17,8 @@ import com.peterAlves.apiVendas.domain.enums.EstadoPagamento;
 import com.peterAlves.apiVendas.repositories.ItemPedidoRepository;
 import com.peterAlves.apiVendas.repositories.PagamentoRepository;
 import com.peterAlves.apiVendas.repositories.PedidoRepository;
+import com.peterAlves.apiVendas.security.UserSS;
+import com.peterAlves.apiVendas.services.exceptions.AuthorizationException;
 import com.peterAlves.apiVendas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -66,5 +72,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
