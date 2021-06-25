0f725f41f02bf98ad88e62e5605b80ddaf306496
Peterson Alves dos Santos <peterhospdis@gmail.com>
2021-06-25 13:11:23 -0300

    Implementando SmtpEmailService com servidor do Google/ Enviando e-mail em HTML

--- src/main/java/com/peterAlves/apiVendas/config/DevConfig.java
+++ src/main/java/com/peterAlves/apiVendas/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.peterAlves.apiVendas.services.DBService;
+import com.peterAlves.apiVendas.services.EmailService;
+import com.peterAlves.apiVendas.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
