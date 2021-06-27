44f6a5ad3acce9d67dc472b28388efe9e2b1942d
Peterson Alves dos Santos <peterhospdis@gmail.com>
2021-06-27 20:36:56 -0300

    Enviando imagem via endpoint

--- src/main/java/com/peterAlves/apiVendas/ApiVendasApplication.java
+++ src/main/java/com/peterAlves/apiVendas/ApiVendasApplication.java
@@ -1,17 +1,11 @@
 package com.peterAlves.apiVendas;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.peterAlves.apiVendas.services.S3Service;
-
 @SpringBootApplication
 public class ApiVendasApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiVendasApplication.class, args);
@@ -19,7 +13,7 @@ public class ApiVendasApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
+
 	}
 
 }
